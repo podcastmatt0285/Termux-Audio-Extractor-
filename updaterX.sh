59c6655f84593a8789deddc7093068c7661e4c71
podcastmatt0285 <podcastmatt0285@gmail.com>
2024-11-15 14:10:22 -0700

    Update updaterX.sh

--- updaterX.sh
+++ updaterX.sh
@@ -5,6 +5,8 @@ cd ~/Termux-Audio-Extractor-
 
 # Discard any local changes to Extractor.sh
 git checkout -- Extractor.sh
+git checkout -- updateX.sh
+git checkout -- vidX.sh
 
 # Pull the latest changes from the repository
 git pull
