99ffde7e7b3060b5879c5bf7cafbd8ca4c06a2aa
podcastmatt0285 <podcastmatt0285@gmail.com>
2024-11-17 21:00:46 -0700

    Update updaterX.sh

--- updaterX.sh
+++ updaterX.sh
@@ -38,6 +38,7 @@ cd ~/Termux-Audio-Extractor-
 git checkout -- Extractor.sh
 git checkout -- updaterX.sh
 git checkout -- vidX.sh
+git checkout -- zipX.sh
 
 # Pull the latest changes from the repository
 git pull
@@ -46,14 +47,17 @@ git pull
 cp Extractor.sh ~/.shortcuts/
 cp updaterX.sh ~/.shortcuts/
 cp vidX.sh ~/.shortcuts/
+cp zipX.sh ~/.shortcuts/
 
 # Ensure the scripts are executable
 chmod +x ~/.shortcuts/Extractor.sh
 chmod +x ~/.shortcuts/updaterX.sh
 chmod +x ~/.shortcuts/vidX.sh
+chmod +x ~/.shortcuts/zipX.sh
 chmod +x Extractor.sh
 chmod +x updaterX.sh
 chmod +x vidX.sh
+chmod +x zipX.sh
 
 # Ensure storage permissions are granted 
 cd ~/storage
