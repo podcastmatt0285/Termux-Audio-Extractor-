cf88a99404bd82bdea80f48e85c4b1f9d76ac9e1
Matthew Wadsworth Carter <podcastmatt0285@gmail.com>
2024-12-02 21:20:43 -0700

    Update updaterX.sh

--- updaterX.sh
+++ updaterX.sh
@@ -43,6 +43,7 @@ git checkout -- syncX.sh
 git checkout -- compressX.sh
 git checkout -- dcomX.sh
 git checkout -- shareX.sh
+git checkout -- messageX.py
 
 # Pull the latest changes from the repository
 git pull
@@ -56,6 +57,7 @@ cp syncX.sh ~/.shortcuts/
 cp compressX.sh ~/.shortcuts/
 cp dcomX.sh ~/.shortcuts/
 cp shareX.sh ~/.shortcuts/
+cp messageX.py ~/.shortcuts
 
 # Ensure the scripts are executable
 chmod +x ~/.shortcuts/Extractor.sh
@@ -66,6 +68,7 @@ chmod +x ~/.shortcuts/syncX.sh
 chmod +x ~/.shortcuts/compressX.sh
 chmod +x ~/.shortcuts/dcomX.sh
 chmod +x ~/.shortcuts/shareX.sh
+chmod +x ~/.shortcuts/messageX.py
 chmod +x Extractor.sh
 chmod +x updaterX.sh
 chmod +x vidX.sh
@@ -74,6 +77,7 @@ chmod +x syncX.sh
 chmod +x compressx.sh
 chmod +x dcomx.sh
 chmod +x shareX.sh
+chmod +x messageX.py
 
 # Ensure storage permissions are granted 
 cd ~/storage
